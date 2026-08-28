f035f98f9160d2391fe5e392d4e1bf0b64c26c8d
jitnesh verma <jitneshverma@jitneshs-MacBook-Air.local>
2026-08-28 21:23:24 +0530

    update the code

--- utile/DB.js
+++ utile/DB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.Mongo_Url);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
