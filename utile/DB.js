31c45a21fcbcd6d58a116921309534bfe5bcff35
jitnesh verma <jitneshverma@jitneshs-MacBook-Air.local>
2026-08-28 21:17:54 +0530

    update mongose file

--- utile/DB.js
+++ utile/DB.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
-const connectDB=async ()=>{
-    try{
-        await mongoose.connect(process.env.Mongo_Url,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-       
-        console.log("connection is success fully")
-    }catch(err){
-        console.log("Error in connection MongoDB")
-    }
-}
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
